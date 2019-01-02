7e482b164071619c6d13ef608a7200403128a747
sarahthieleman <42797978+sarahthieleman@users.noreply.github.com>
2019-01-02 03:45:01 +0100

    Add files via upload

---
+++ src/Views/BeachView.ts
@@ -0,0 +1,50 @@
+class BeachView extends BaseView {
+
+    public constructor (src : string,
+                        canvas : Canvas,
+                        player : Player,
+                        mouseListener : MouseHelper) {
+        super(src, canvas, player, mouseListener);
+    };
+
+    /**
+     * @access public
+     * @method
+     * Required method to draw canvas
+     */
+    public draw = () => {
+        this.canvas.drawImageToCanvas(  this.src,
+                                        0,
+                                        0,
+                                        this.canvas.getWidth(),
+                                        this.canvas.getHeight());
+        this.canvas.drawButtonToCanvas(  "./assets/Icons/ButtonsFree/Home.png",
+                                        this.canvas.getWidth() * 0.05,
+                                        this.canvas.getHeight() * 0.05,
+                                        this.canvas.getWidth() * 0.025,
+                                        this.canvas.getHeight() * 0.05, 
+                                        () => {
+                                            this.player.setLocation("Map");
+                                            this.mouseListener.setHasBeenClicked()});
+        this.canvas.drawButtonToCanvas(  "./assets/FishingGame/boat.png",
+                                        this.canvas.getWidth() * 0.339,
+                                        this.canvas.getHeight() * 0.2555,
+                                        this.canvas.getWidth()*0.24,
+                                        this.canvas.getHeight()*0.32, 
+                                        () => {
+                                            if(this.player.getEnergy()>=15&&
+                                            this.player.getHunger()>=30){
+                                            this.player.setLocation("Fishing")
+                                        };
+                                            this.mouseListener.setHasBeenClicked()});
+        this.canvas.drawCoinToCanvas(   this.canvas.getWidth()/2,
+                                        this.canvas.getHeight() * 0.04,
+                                        this.player.getCoin());
+        this.canvas.drawBarstoCanvas(   this.canvas.getWidth()*0.9,
+                                        this.canvas.getHeight()*0.05,
+                                        this.player.getHunger(),
+                                        this.player.getEnergy(),
+                                        this.player.getMood(),
+                                        this.player.getHealth());
+    };
+};
