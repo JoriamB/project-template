8add4b8fbc9bc278662e189fd649af3e389c4c4f
Anfaza <a.frayhi@gmail.com>
2019-01-10 13:40:04 +0100

    Test

--- build/app.js
+++ build/app.js
@@ -2006,14 +2006,14 @@ class SelectPlayer extends BaseView {
         super(src, canvas, player, mouseListener);
         this.draw = () => {
             this.canvas.drawImageToCanvas(this.src, 0, 0, this.canvas.getWidth(), this.canvas.getHeight());
-            this.canvas.drawTextToCanvas("center", 60, "Minecraft", "orange", "Ludos Mundi", this.canvas.getWidth() * 0.5, this.canvas.getHeight() * 0.2);
-            this.canvas.drawTextToCanvas("center", 40, "Minecraft", "white", "Kies je karakter.", this.canvas.getWidth() * 0.5, this.canvas.getHeight() * 0.4);
-            this.canvas.drawButtonToCanvas("./Assets/Player/Female/Poses/female_idle.png", this.canvas.getWidth() * 0.35, this.canvas.getHeight() * 0.5, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.2, () => {
+            this.canvas.drawTextToCanvas("center", 100, "Minecraft", "orange", "Ludos Mundi", this.canvas.getWidth() * 0.5, this.canvas.getHeight() * 0.25);
+            this.canvas.drawTextToCanvas("center", 50, "Minecraft", "white", "Kies je karakter", this.canvas.getWidth() * 0.5, this.canvas.getHeight() * 0.5);
+            this.canvas.drawButtonToCanvas("./Assets/Player/Female/Poses/female_idle.png", this.canvas.getWidth() * 0.35, this.canvas.getHeight() * 0.55, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.2, () => {
                 this.player.setSrc("./Assets/Player/Female/Poses/female_idle.png");
                 this.player.setLocation("Map");
                 this.mouseListener.setHasBeenClicked();
             });
-            this.canvas.drawButtonToCanvas("./Assets/Player/Male/Poses/player_idle.png", this.canvas.getWidth() * 0.55, this.canvas.getHeight() * 0.5, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.2, () => {
+            this.canvas.drawButtonToCanvas("./Assets/Player/Male/Poses/player_idle.png", this.canvas.getWidth() * 0.55, this.canvas.getHeight() * 0.55, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.2, () => {
                 this.player.setSrc("./Assets/Player/Male/Poses/player_idle.png");
                 this.player.setLocation("Map");
                 this.mouseListener.setHasBeenClicked();
@@ -2062,11 +2062,15 @@ class StartView extends BaseView {
         this.draw = () => {
             this.canvas.drawImageToCanvas(this.src, 0, 0, this.canvas.getWidth(), this.canvas.getHeight());
             this.canvas.drawTextToCanvas("center", 100, "Minecraft", "orange", "Ludos Mundi", this.canvas.getWidth() * 0.5, this.canvas.getHeight() * 0.25);
-            this.canvas.drawTextButtonToCanvas("./Assets/Icons/ButtonsFREE/PlayBlank.png", "Uitleg", this.canvas.getWidth() * 0.4 - (this.canvas.getWidth() * 0.1) / 2, this.canvas.getHeight() * 0.5 - (this.canvas.getHeight() * 0.1) / 2, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.1, () => {
+            this.canvas.drawTextButtonToCanvas("./Assets/Icons/ButtonsFREE/PlayBlank.png", "Uitleg", this.canvas.getWidth() * 0.4 - (this.canvas.getWidth() * 0.1) / 2, this.canvas.getHeight() * 0.8 - (this.canvas.getHeight() * 0.1) / 2, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.1, () => {
                 this.player.setLocation("Tutorial");
                 this.mouseListener.setHasBeenClicked();
             });
-            this.canvas.drawTextButtonToCanvas("./Assets/Icons/ButtonsFREE/PlayBlank.png", "Speel", this.canvas.getWidth() * 0.6 - (this.canvas.getWidth() * 0.1) / 2, this.canvas.getHeight() * 0.5 - (this.canvas.getHeight() * 0.1) / 2, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.1, () => {
+            this.canvas.drawTextButtonToCanvas("./Assets/Icons/ButtonsFREE/PlayBlank.png", "Speel", this.canvas.getWidth() * 0.6 - (this.canvas.getWidth() * 0.1) / 2, this.canvas.getHeight() * 0.8 - (this.canvas.getHeight() * 0.1) / 2, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.1, () => {
+                this.player.setLocation("SelectPlayer");
+                this.mouseListener.setHasBeenClicked();
+            });
+            this.canvas.drawButtonToCanvas("./Assets/Icons/ButtonsFREE/globe.png", this.canvas.getWidth() * 0.45, this.canvas.getHeight() * 0.4, this.canvas.getWidth() * 0.1, this.canvas.getHeight() * 0.2, () => {
                 this.player.setLocation("SelectPlayer");
                 this.mouseListener.setHasBeenClicked();
             });
@@ -2112,6 +2116,7 @@ class TutorialView extends BaseView {
             this.canvas.drawTextToCanvas("left", 35, "KenneyPixel", "white", "Om het poppetje naar links te bewegen gebruik je A of <", this.canvas.getWidth() * 0.05, this.canvas.getHeight() * 0.225);
             this.canvas.drawTextToCanvas("left", 35, "KenneyPixel", "white", "Om het poppetje naar onder te bewegen gebruik je S of ∨", this.canvas.getWidth() * 0.05, this.canvas.getHeight() * 0.25);
             this.canvas.drawTextToCanvas("left", 35, "KenneyPixel", "white", "Om het poppetje naar rechts te bewegen gebruik je D of >", this.canvas.getWidth() * 0.05, this.canvas.getHeight() * 0.275);
+            this.canvas.drawTextToCanvas("left", 35, "KenneyPixel", "white", "Klik op de locaties om de mogelijkheden te ontdekken!", this.canvas.getWidth() * 0.05, this.canvas.getHeight() * 0.4);
         };
     }
     ;
