1db010f81c2ddd2cb8b6dc9961bda9fd82e05e2f
Anfaza <a.frayhi@gmail.com>
2019-01-10 14:32:37 +0100

    tutorial completed

--- src/Views/TutorialView.ts
+++ src/Views/TutorialView.ts
@@ -29,34 +29,62 @@ this.canvas.drawTextToCanvas("left",
                             "white",
                             "Om het poppetje naar boven te bewegen gebruik je W of ^",
                             this.canvas.getWidth()*0.05,
-                            this.canvas.getHeight()*0.2);
+                            this.canvas.getHeight()*0.45);
 this.canvas.drawTextToCanvas("left",
                             35,
                             "KenneyPixel",
                             "white",
                             "Om het poppetje naar links te bewegen gebruik je A of <",
                             this.canvas.getWidth()*0.05,
-                            this.canvas.getHeight()*0.225);
+                            this.canvas.getHeight()*0.5);
 this.canvas.drawTextToCanvas("left",
                             35,
                             "KenneyPixel",
                             "white",
                             "Om het poppetje naar onder te bewegen gebruik je S of ∨",
                             this.canvas.getWidth()*0.05,
-                            this.canvas.getHeight()*0.25);
+                            this.canvas.getHeight()*0.55);
 this.canvas.drawTextToCanvas("left",
                             35,
                             "KenneyPixel",
                             "white",
                             "Om het poppetje naar rechts te bewegen gebruik je D of >",
                             this.canvas.getWidth()*0.05,
-                            this.canvas.getHeight()*0.275);
+                            this.canvas.getHeight()*0.6);
 this.canvas.drawTextToCanvas("left",
                             35,
                             "KenneyPixel",
                             "white",
                             "Klik op de locaties om de mogelijkheden te ontdekken!",
-                            this.canvas.getWidth()*0.05,
-                            this.canvas.getHeight()*0.4);                                                     
+                            this.canvas.getWidth()*0.5,
+                            this.canvas.getHeight()*0.45)
+this.canvas.drawTextToCanvas("left",
+                            35,
+                            "KenneyPixel",
+                            "white",
+                            "Voltooi je dagelijkse taken!",
+                            this.canvas.getWidth()*0.5,
+                            this.canvas.getHeight()*0.5); 
+this.canvas.drawTextToCanvas("left",
+                            35,
+                            "KenneyPixel",
+                            "white",
+                            "En vergeet niet je behoeftes in de gaten te houden!",
+                            this.canvas.getWidth()*0.5,
+                            this.canvas.getHeight()*0.55); 
+this.canvas.drawTextToCanvas("center",
+                            70,
+                            "KenneyPixel",
+                            "white",
+                            "Tips",
+                            this.canvas.getWidth()*0.57,
+                            this.canvas.getHeight()*0.2); 
+this.canvas.drawTextToCanvas("center",
+                            70,
+                            "KenneyPixel",
+                            "white",
+                            "Controls",
+                            this.canvas.getWidth()*0.2,
+                            this.canvas.getHeight()*0.2);                                                    
+};                                                                                
 };
-}
